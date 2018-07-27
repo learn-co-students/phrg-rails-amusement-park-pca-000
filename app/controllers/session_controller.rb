af3ff51d936b6db8d4af3afeb5b5b510ac4fd821
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-07-27 19:55:07 -0400

    pre rubocop

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,18 @@
+class SessionController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # binding.pry
+    user = User.find_by_id(params[:user][:name])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to "/"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if nausea && happiness
+      nausea > happiness ? "sad" : "happy"
+    end
+  end
+
+  def user_name=(name)
+    self.name = name
+  end
+
+  def user_name
+    name
+  end
+
 end
