bcc28cb4a8b38841f2474e921c7ad37c7364c3eb
Katie Weber <k.weber0893@gmail.com>
2018-07-30 13:07:41 -0400

    Set up models and migrations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,43 @@
 # frozen_string_literal: true
 
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if not_enough_tickets? && not_tall_enough?
+      tickets_denied + " " + height_denied
+    elsif not_tall_enough?
+      "Sorry. " + height_denied
+    elsif not_enough_tickets?
+      tickets_denied
+    else
+      ride_the_ride
+    end
+  end
+
+private
+
+  def tickets_denied
+    "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+  end
+
+  def height_denied
+    "You are not tall enough to ride the #{attraction.name}."
+  end
+
+  def not_enough_tickets?
+    true if user.tickets < attraction.tickets
+  end
+
+  def not_tall_enough?
+    true if user.height < attraction.min_height
+  end
+
+  def ride_the_ride
+    user.tickets -= attraction.tickets
+    user.nausea += attraction.nausea_rating
+    user.happiness += attraction.happiness_rating
+    user.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if nausea > happiness && nausea != happiness
+      "sad"
+    elsif nausea < happiness && nausea != happiness
+      "happy"
+    end
+  end
 end
