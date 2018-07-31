7fca36a495864e29d67c9f844753030746b1d963
Katie Weber <k.weber0893@gmail.com>
2018-07-30 20:41:22 -0400

    Completed sign out flow

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,4 +10,9 @@ class SessionController < ApplicationController
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   end
+
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to "/"
+  end
 end
